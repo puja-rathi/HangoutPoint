b3db8a79618d7c781184f34bbec308b8ee067187
puja-rathi <pdjajal20@gmail.com>
2025-07-28 16:25:17 +0000

    index.js and jenkinsfile updated with spin up prometheus stage

--- nodejs_app_code/index.js
+++ nodejs_app_code/index.js
@@ -2,6 +2,40 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello from Hangout Point!'));
+const client = require('prom-client');
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// Create a Registry to register the metrics
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+// Sample custom metric (optional)
+const counter = new client.Counter({
+  name: 'app_request_count',
+  help: 'Total number of requests',
+});
+register.registerMetric(counter);
+
+// Middleware to count all requests
+app.use((req, res, next) => {
+  counter.inc();
+  next();
+});
+
+// Main route
+app.get('/', (req, res) => {
+  res.send('Hello from HangoutPoint!');
+});
+
+// Metrics endpoint
+app.get('/metrics', async (req, res) => {
+  try {
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+  } catch (ex) {
+    res.status(500).end(ex);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
